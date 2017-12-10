ca9048953c795c9cabbeedfaccf720146159a08c
Sarah <sarah.hodash@gmail.com>
2017-12-10 17:59:09 -0500

    Homework 4

---
+++ Class Work/class07/first-javascript/script.js
@@ -0,0 +1,20 @@
+var product = 10*10;
+var quotient = 100/10;
+var sum = 50+17;
+var name = 'Sarah' + ' Hodash';
+
+console.log(product,quotient + sum + name);
+
+function onra () {
+    var city = 'Saigon';
+
+    console.log(`Last Tango In ${city}`)
+}
+
+onra();
+onra();
+onra();
+
+var hello = document.getElementById('hello');
+
+hello.style.color = 'tomato';
